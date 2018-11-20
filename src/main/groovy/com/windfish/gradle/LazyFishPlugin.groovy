0b862d75991f11462c8d8db32ec57dbdbc7d4c29
KenHuang <hwp0415229@sina.cn>
2018-11-20 16:00:58 +0800

    修复id plugin导入插件的方式无法使用的问题
    Change-Id: I86406122231cb559ae4a5f3499c1254b2e411fe8

--- src/main/groovy/com/windfish/gradle/LazyFishPlugin.groovy
+++ src/main/groovy/com/windfish/gradle/LazyFishPlugin.groovy
@@ -7,29 +7,59 @@ import com.windfish.gradle.automatics.JavaWebAutomatics
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.plugins.PluginContainer
+import org.gradle.api.plugins.PluginManager
 
 /**
  * Created by kenhuang on 2018/10/27.
  */
 public class LazyFishPlugin implements Plugin<Project> {
     public static final String GROUP = "lazyfish"
+
     @Override
     void apply(Project project) {
-        project.afterEvaluate {
-            PluginContainer pluginContainer = project.getPlugins()
-            def automatics = null
-            if (pluginContainer.hasPlugin("war")) {
+        /*不能在afterEvaluate执行*/
+        PluginManager pluginManager = project.getPluginManager()
+        def automatics = null
+        /*id引入插件时以下代码无效*/
+//        if (pluginManager.hasPlugin("war")) {
+//            automatics = new JavaWebAutomatics(project)
+//        } else if (pluginManager.hasPlugin("android")) {
+//            automatics = new AndroidAutomatics(project)
+//        } else if (pluginManager.hasPlugin("hadoop")) {
+//            automatics = new HadoopAutomatics(project)
+//        } else if (pluginManager.hasPlugin("java")) {
+//            automatics = new JavaAutomatics(project)
+//        } else {
+//            throw new Exception("Unknown project type!This plugin should be applied after plugin java,android or war!")
+//        }
+
+        pluginManager.withPlugin('war'){
+            if (automatics == null || automatics instanceof JavaAutomatics){
                 automatics = new JavaWebAutomatics(project)
-            } else if (pluginContainer.hasPlugin("android")) {
+            }
+        }
+
+        pluginManager.withPlugin('android'){
+            if (automatics == null || automatics instanceof JavaAutomatics){
                 automatics = new AndroidAutomatics(project)
-            } else if (pluginContainer.hasPlugin("hadoop")) {
+            }
+        }
+        pluginManager.withPlugin('hadoop'){
+            if (automatics == null || automatics instanceof JavaAutomatics) {
                 automatics = new HadoopAutomatics(project)
-            } else if (pluginContainer.hasPlugin("java")) {
+            }
+        }
+        pluginManager.withPlugin('java'){
+            if (automatics == null) {
                 automatics = new JavaAutomatics(project)
-            } else {
+            }
+        }
+        project.afterEvaluate {
+            if (automatics != null){
+                automatics.execute()
+            }else{
                 throw new Exception("Unknown project type!This plugin should be applied after plugin java,android or war!")
             }
-            automatics.execute()
         }
     }
 }
